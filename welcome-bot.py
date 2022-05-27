4ea1b9e662d6258ec4d63557526e2f7c3997b035
Sudhanva Nagaragadde <sudhanva@Sudhanvas-MacBook-Air.local>
2022-05-27 18:26:39 -0400

    local

--- welcome-bot.py
+++ welcome-bot.py
@@ -65,8 +65,12 @@ def index():
 		print ('real msg')
 		msg = getMsg(json_content['data']['id'])
 		email = json_content['data']['personEmail']
+
+		person = webex_person(email)
+
 		person = getPerson(Persons, email)
-		
+		Persons.append(person)
+
 		print ('Create a container!')
 		sendMsg(person.email,  'Hello! Do you want to play a game? Remeber I am just a yes/no bot but you can say "start" to startover or "quit" to end anytime')
 
