a7eb02aa67176f98da5ef61ae7b0c873f3073c56
Sudhanva Nagaragadde <sudhanva@Sudhanvas-MacBook-Air.local>
2022-06-03 12:03:14 -0400

    local

---
+++ welcome-bot/webex_person.py
@@ -0,0 +1,14 @@
+
+class webex_person:
+
+	def __init__(self,email):
+		self.email = email
+		self.person = None
+		self.roomId = None
+		self.msg = None
+		self.Questions = []
+		self.AskQues = 1
+		self.container = email.replace('@','.')
+		self.port = None
+
+
