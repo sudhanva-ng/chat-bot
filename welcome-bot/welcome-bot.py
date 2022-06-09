058106c4e069270dcb3688523168551f2f86cb00
Sudhanva Nagaragadde <sudhanva@Sudhanvas-MacBook-Air.local>
2022-06-08 23:23:22 -0400

    local

--- welcome-bot/welcome-bot.py
+++ welcome-bot/welcome-bot.py
@@ -68,7 +68,7 @@ def forwardApi(ip, json_content, port):
 
 def createContainer(name, port):
 
-	base = 'ssh -i /root/chat-bot.pem ubuntu@172.17.0.1 '
+	base = 'ssh -o StrictHostKeychecking=no -i /root/chat-bot.pem ubuntu@172.17.0.1 '
 	cmd = 'sudo docker run -d -p {}:{} -e CONT_PORT={} --name {} worker-bot'.format(port, port, port, name)
 	out = check_output((base + cmd).split())
 
